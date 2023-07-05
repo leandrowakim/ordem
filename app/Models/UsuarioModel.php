ba30a5de8d08083ab70ba7c03effd65c07780d50
Leandro Wakim <leandrowakim@gmail.com>
2023-07-05 15:45:21 -0300

    Update UsuarioModel.php

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
 use App\Libraries\Token;
+use CodeIgniter\Model;
 
 class UsuarioModel extends Model
 {
-    protected $table            = 'usuarios';
-    protected $returnType       = 'App\Entities\Usuario';
-    protected $useSoftDeletes   = true;
-    protected $allowedFields    = [
+    protected $table = 'usuarios';
+    protected $returnType = 'App\Entities\Usuario';
+    protected $useSoftDeletes = true;
+    protected $allowedFields = [
         'nome',
         'email',
         'password',
@@ -21,28 +21,28 @@ class UsuarioModel extends Model
 
     // Dates
     protected $useTimestamps = true;
-    protected $createdField  = 'criado_em';
-    protected $updatedField  = 'atualizado_em';
-    protected $deletedField  = 'deletado_em';
+    protected $createdField = 'criado_em';
+    protected $updatedField = 'atualizado_em';
+    protected $deletedField = 'deletado_em';
 
     // Validation
     protected $validationRules = [
         'nome' => 'required|min_length[3]|max_length[125]',
         'email' => 'required|valid_email|max_length[230]|is_unique[usuarios.email,id,{id}]',
         'password' => 'required|min_length[6]',
-        'password_confirmation' => 'required_with[password]|matches[password]'
+        'password_confirmation' => 'required_with[password]|matches[password]',
     ];
 
     protected $validationMessages = [
         'nome' => [
             'required' => 'O campo Nome é Obrigatório!',
             'min_length' => 'O campo Nome deve ser maior que 3 caractéres!',
-            'max_length' => 'O campo Nome não pode ser maior que 125 caractéres!'
+            'max_length' => 'O campo Nome não pode ser maior que 125 caractéres!',
         ],
         'email' => [
             'required' => 'O campo E-mail é Obrigatório!',
             'max_length' => 'O campo E-mail não pode ser maior que 230 caractéres!',
-            'is_unique' => 'Esse E-mail já existe, tente outro!'
+            'is_unique' => 'Esse E-mail já existe, tente outro!',
         ],
         'password' => [
             'required' => 'O campo Senha é Obrigatório!',
@@ -50,13 +50,13 @@ class UsuarioModel extends Model
         ],
         'password_confirmation' => [
             'required_with' => 'Por favor confirme a senha!',
-            'matches' => 'As senhas precisam combinar!'
+            'matches' => 'As senhas precisam combinar!',
         ],
     ];
 
     // Callbacks
-    protected $beforeInsert   = ['hashPassword'];
-    protected $beforeUpdate   = ['hashPassword'];
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
 
     protected function hashPassword(array $data)
     {
@@ -67,18 +67,17 @@ class UsuarioModel extends Model
             unset($data['data']['password']);
             unset($data['data']['password_confirmation']);
 
-        }    
+        }
 
         return $data;
-    }    
-    
-    public function buscaUsuarioPorEmail(string $email)
-    {
-        return $this->where('email', $email)
-                    ->where('deletado_em', null)
-                    ->first();
     }
 
+    public function buscaUsuarioPorEmail(string $email = null)
+    {
+        return $this->where('email', $email)
+            ->where('deletado_em', null)
+            ->first();
+    }
 
     /**
      * Método que busca o usuário de acordo com o hash do token
@@ -93,21 +92,21 @@ class UsuarioModel extends Model
         $tokenHash = $token->getHash();
 
         $usuario = $this->where('reset_hash', $tokenHash)
-                        ->where('deletado_em', null)
-                        ->first();
+            ->where('deletado_em', null)
+            ->first();
 
         if ($usuario === null) {
-            
+
             return null;
         }
 
         if ($usuario->reset_expira_em < date('Y-m-d H:i:s')) {
-            
+
             return null;
         }
 
         return $usuario;
-    }    
+    }
 
     /**
      * Método que recupera as permissões do usuário logado
@@ -125,30 +124,30 @@ class UsuarioModel extends Model
         ];
 
         return $this->select($atributos)
-                    ->asArray()
-                    ->join('grupos_usuarios', 'grupos_usuarios.usuario_id = usuarios.id')
-                    ->join('grupos_permissoes', 'grupos_permissoes.grupo_id = grupos_usuarios.grupo_id')
-                    ->join('permissoes', 'permissoes.id = grupos_permissoes.permissao_id')
-                    ->where('usuarios.id', $usuario_id)
-                    ->groupBy('permissoes.nome')
-                    ->findAll();
+            ->asArray()
+            ->join('grupos_usuarios', 'grupos_usuarios.usuario_id = usuarios.id')
+            ->join('grupos_permissoes', 'grupos_permissoes.grupo_id = grupos_usuarios.grupo_id')
+            ->join('permissoes', 'permissoes.id = grupos_permissoes.permissao_id')
+            ->where('usuarios.id', $usuario_id)
+            ->groupBy('permissoes.nome')
+            ->findAll();
     }
-    
+
     public function atualizaEmailDoCliente(int $usuario_id, string $email)
     {
         return $this->protect(false)
-                    ->where('id', $usuario_id)
-                    ->set('email', $email)
-                    ->update();
+            ->where('id', $usuario_id)
+            ->set('email', $email)
+            ->update();
     }
-    
+
     /**
      * Método responsável por recuperar os técnicos para serem exibidos como opção de definição do mesmo para a OS
      *
      * @param string $termo
      * @return null|array
      */
-    public function recuperaResponsaveisParaOrdem(string $termo = null)    
+    public function recuperaResponsaveisParaOrdem(string $termo = null)
     {
         if ($termo === null) {
             return [];
@@ -160,16 +159,16 @@ class UsuarioModel extends Model
         ];
 
         $responsaveis = $this->select($atributos)
-                             ->join('grupos_usuarios', 'grupos_usuarios.usuario_id = usuarios.id')
-                             ->join('grupos', 'grupos.id = grupos_usuarios.grupo_id')
-                             ->like('usuarios.nome', $termo)
-                             ->where('usuarios.ativo', true)
-                             ->where('usuarios.deletado_em', null)
-                             ->where('grupos.exibir', true)
-                             ->where('grupos.deletado_em', null)
-                             ->where('grupos.id !=', 2)
-                             ->groupBy('usuarios.nome')
-                             ->findAll();
+            ->join('grupos_usuarios', 'grupos_usuarios.usuario_id = usuarios.id')
+            ->join('grupos', 'grupos.id = grupos_usuarios.grupo_id')
+            ->like('usuarios.nome', $termo)
+            ->where('usuarios.ativo', true)
+            ->where('usuarios.deletado_em', null)
+            ->where('grupos.exibir', true)
+            ->where('grupos.deletado_em', null)
+            ->where('grupos.id !=', 2)
+            ->groupBy('usuarios.nome')
+            ->findAll();
 
         if ($responsaveis === null) {
             return [];
@@ -192,14 +191,14 @@ class UsuarioModel extends Model
         $where = 'ordens.criado_em BETWEEN "' . $dataInicial . '" AND "' . $dataFinal . '"';
 
         return $this->select($atributos)
-                    ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_abertura_id=usuarios.id')
-                    ->join('ordens', 'ordens.id=ordens_responsaveis.ordem_id')
-                    ->where($where)
-                    ->withDeleted(true) //recupera os usuários já marcados como deletado
-                    ->groupBy('usuarios.nome')
-                    ->orderBy('qtde_ordens', 'DESC')
-                    //->getCompiledSelect();
-                    ->findAll();
+            ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_abertura_id=usuarios.id')
+            ->join('ordens', 'ordens.id=ordens_responsaveis.ordem_id')
+            ->where($where)
+            ->withDeleted(true) //recupera os usuários já marcados como deletado
+            ->groupBy('usuarios.nome')
+            ->orderBy('qtde_ordens', 'DESC')
+        //->getCompiledSelect();
+            ->findAll();
     }
 
     public function recuperaResponsaveisParaRelatorio(string $dataInicial, string $dataFinal)
@@ -216,15 +215,15 @@ class UsuarioModel extends Model
         $where = 'ordens.atualizado_em BETWEEN "' . $dataInicial . '" AND "' . $dataFinal . '"';
 
         return $this->select($atributos)
-                    ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_responsavel_id=usuarios.id')
-                    ->join('ordens', 'ordens.id=ordens_responsaveis.ordem_id')
-                    ->where($where)
-                    ->where('ordens.situacao !=', 'aberta')
-                    ->withDeleted(true) //recupera os usuários já marcados como deletado
-                    ->groupBy('ordens_responsaveis.usuario_responsavel_id')
-                    ->orderBy('qtde_ordens', 'DESC')
-                    //->getCompiledSelect();
-                    ->findAll();
+            ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_responsavel_id=usuarios.id')
+            ->join('ordens', 'ordens.id=ordens_responsaveis.ordem_id')
+            ->where($where)
+            ->where('ordens.situacao !=', 'aberta')
+            ->withDeleted(true) //recupera os usuários já marcados como deletado
+            ->groupBy('ordens_responsaveis.usuario_responsavel_id')
+            ->orderBy('qtde_ordens', 'DESC')
+        //->getCompiledSelect();
+            ->findAll();
     }
 
     public function recuperaUsuariosParaLog($termo)
@@ -240,21 +239,21 @@ class UsuarioModel extends Model
         $atributos = [
             'usuarios.id',
             'usuarios.nome',
-            'usuarios.email'
+            'usuarios.email',
         ];
 
         if (empty($clientesUsuariosIds)) {
             return $this->select($atributos)
-                        ->withDeleted(true)
-                        ->like('usuarios.nome', $termo)
-                        ->findAll();
+                ->withDeleted(true)
+                ->like('usuarios.nome', $termo)
+                ->findAll();
         }
 
         return $this->select($atributos)
-                    ->whereNotIn('usuarios.id', $clientesUsuariosIds)
-                    ->withDeleted(true)
-                    ->like('usuarios.nome', $termo)
-                    ->findAll();
+            ->whereNotIn('usuarios.id', $clientesUsuariosIds)
+            ->withDeleted(true)
+            ->like('usuarios.nome', $termo)
+            ->findAll();
     }
 
     public function recuperaAtendentesGrafico(string $anoEscolhido)
@@ -268,13 +267,13 @@ class UsuarioModel extends Model
         ];
 
         return $this->select($atributos)
-                    ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_abertura_id = usuarios.id')
-                    ->join('ordens', 'ordens.id = ordens_responsaveis.ordem_id')
-                    ->where('YEAR(ordens.criado_em)', $anoEscolhido)
-                    ->withDeleted(true)
-                    ->groupBy('usuarios.nome')
-                    ->orderBy('qtde_os', 'DESC')
-                    ->findAll();
+            ->join('ordens_responsaveis', 'ordens_responsaveis.usuario_abertura_id = usuarios.id')
+            ->join('ordens', 'ordens.id = ordens_responsaveis.ordem_id')
+            ->where('YEAR(ordens.criado_em)', $anoEscolhido)
+            ->withDeleted(true)
+            ->groupBy('usuarios.nome')
+            ->orderBy('qtde_os', 'DESC')
+            ->findAll();
     }
 
 }
